7a2efa2e4d30cd7073ba7b99f65a706a73d07619
Vikas Sani <105963100+CoderVikas21@users.noreply.github.com>
2024-04-03 11:59:58 +0530

    F

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='footer'>
-        Made with ❤ by Vikas | <a href="#" target="_blank">Github</a>
+        Made with ❤ by Vikas | <a href="https://trackease01.netlify.app/" target="_blank">Github</a>
     </div>
   )
 }
